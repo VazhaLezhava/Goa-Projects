ca2ae75624c3ef81b651740f4d3349e211052814
VazhaLezhava <vazha566@gmail.com>
2024-01-18 22:23:05 +0300

    second commit

---
+++ Day1/Myportfolio.py
@@ -0,0 +1,10 @@
+name = "Name:vazha"
+lastname = "Lastname:lezhava"
+age = 12
+height = 1,57
+FoodWhatILove = "FoodWhatILove:Shaurma,khinkali"
+MyLove = "MyLove:not your busines -_-"
+family = "family:me","big brother","Dad","mom"
+
+print (name,lastname,age,height,height,MyLove,family)
+
